0eb586e36e84562a5ad9b30b88d6d111fad70c1d
heyanbai <heyanbai@yahoo.com>
2021-08-14 19:42:08 +0800

    add assets

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+<!--    <img alt="Vue logo" src="../assets/logo.png">-->
+    <FlipCard />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import FlipCard from "../components/FlipCard";
+export default {
+  name: 'Home',
+  components: {
+    FlipCard
+  }
+}
+</script>
